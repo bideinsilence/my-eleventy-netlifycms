63c8cb3b96131916b2b3f0d44f1f72afdeefcb48
Corey Scanlan <bideinsilence@gmail.com>
2021-02-15 14:31:22 -0800

    added luxon to format readable dates in posts

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,28 @@
 
 module.exports = function(eleventyConfig) {
 
-    // Copy images to /_site/images
-    eleventyConfig.addPassthroughCopy("images");
- 
-    // NetlifyCMS
-    // Copy NetlifyCMS admin to /_site/admin
-    eleventyConfig.addPassthroughCopy('admin');
+  // Copy images to /_site/images
+  eleventyConfig.addPassthroughCopy("images");
 
+  // NetlifyCMS
+  // Copy NetlifyCMS admin to /_site/admin
+  eleventyConfig.addPassthroughCopy('admin');
+  
+  // For date modification
+  const {
+    DateTime
+  } = require("luxon");
 
+  // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+  eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat('yy-MM-dd');
+  });
+
+  eleventyConfig.addFilter("readableDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {
+      zone: 'utc'
+    }).toFormat("dd-MM-yy");
+  });
 }  
